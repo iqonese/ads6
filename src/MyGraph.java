480e3a758c841e026c1b31f33332b4d06cb492fc
iqonese <iqonese@gmail.com>
2023-05-31 23:30:43 +0600

    Shortest path algorithm

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -2,7 +2,8 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
+import java.util.Queue;
+import java.util.*;
 public class MyGraph<Vertex> {
     private Map<Vertex, List<Vertex>> list;
 
@@ -72,6 +73,29 @@ public class MyGraph<Vertex> {
 
     }
 
+    public void BFS(Vertex start) {
+        validateVertex(start);
+        Map<Vertex, Boolean> visited = new HashMap<>();
+        for (Vertex vertex : list.keySet()) {
+            visited.put(vertex, false);
+        }
+
+        Queue<Vertex> queue = new LinkedList<>();
+        visited.put(start, true);
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            Vertex vertex = queue.poll();
+            System.out.print(vertex + " ");
+
+            for (Vertex neighbor : list.get(vertex)) {
+                if (!visited.get(neighbor)) {
+                    visited.put(neighbor, true);
+                    queue.offer(neighbor);
+                }
+            }
+        } }
+
 
     private void DFSHelper(Vertex vertex, Map<Vertex, Boolean> visited) {
         visited.put(vertex, true);
@@ -82,4 +106,57 @@ public class MyGraph<Vertex> {
             }
         }
     }
+
+    public Map<Vertex, Integer> dijkstraShortestPath(Vertex source) {
+        validateVertex(source);
+
+        Map<Vertex, Integer> distance = new HashMap<>();
+        Map<Vertex, Vertex> previous = new HashMap<>();
+        PriorityQueue<Vertex> queue = new PriorityQueue<>(Comparator.comparingInt(distance::get));
+
+        for (Vertex vertex : adjacencyList.keySet()) {
+            distance.put(vertex, Integer.MAX_VALUE);
+            previous.put(vertex, null);
+            queue.offer(vertex);
+        }
+
+        distance.put(source, 0);
+
+        while (!queue.isEmpty()) {
+            Vertex current = queue.poll();
+            List<Edge<Vertex>> neighbors = adjacencyList.get(current);
+            if (neighbors != null) {
+                for (Edge<Vertex> edge : neighbors) {
+                    Vertex neighbor = edge.getDestination();
+                    int newDistance = distance.get(current) + edge.getWeight();
+                    if (newDistance < distance.get(neighbor)) {
+                        distance.put(neighbor, newDistance);
+                        previous.put(neighbor, current);
+                        queue.remove(neighbor);
+                        queue.offer(neighbor);
+                    }
+                }
+            }
+        }
+
+        return distance;
+    }
+    private static class Edge<Vertex> {
+        private Vertex destination;
+        private int weight;
+
+        public Edge(Vertex destination, int weight) {
+            this.destination = destination;
+            this.weight = weight;
+        }
+
+        public Vertex getDestination() {
+            return destination;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+    }
+
 }
